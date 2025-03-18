d045fb7434651db5254a887c9ee3ddf92624e58d
kcza <ed@kcza.net>
2025-03-18 22:00:07 +0000

    Minor cleaning

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 for computer_dir in ~/.local/share/PrismLauncher/instances/CC-\ Islands/.minecraft/saves/*/computercraft/computer/*/; do
-	rsync -a --exclude "*.lua.packed" ./bin "$computer_dir";
-	mv "$computer_dir"/bin/* "$computer_dir";
-	rmdir "$computer_dir"/bin/;
+	rsync -a --exclude "*.lua.packed" ./bin "$computer_dir"
+	mv "$computer_dir"/bin/* "$computer_dir"
+	rmdir "$computer_dir"/bin/
 done
